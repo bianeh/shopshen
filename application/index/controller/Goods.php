9109b0e2f9f5f2d4c939d15096a2a8d42fd2e317
bianenhui <1499845029@qq.com>
2018-06-13 16:03:48 +0800

    2018 0613

--- application/index/controller/Goods.php
+++ application/index/controller/Goods.php
@@ -2,10 +2,14 @@
 namespace app\index\controller;
 use think\Controller;
 
-class Index extends Controller
+class Goods extends Controller
 {
     public function index()
     {
         return $this->fetch();
+    }
+    public function add()
+    {
+    	return $this->fetch();
     }
 }
